1e8979bf24debc6ac4ecfeaef46b918ef0c28ab5
schittimilla <schittimilla@ebay.com>
2019-08-05 15:03:51 -0700

    Adding routing to burger builder and checkout pages

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -87,7 +87,7 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-        this.setState({loading: true});
+        /*this.setState({loading: true});
         const order = {
             ingredients: this.state.ingredients,
             price: this.state.totalPrice,
@@ -108,7 +108,8 @@ class BurgerBuilder extends Component {
             })
             .catch(err => {
                 this.setState({loading: false, purchasing: false});
-            });
+            });*/
+        this.props.history.push('/checkout');
     };
 
     render() {
